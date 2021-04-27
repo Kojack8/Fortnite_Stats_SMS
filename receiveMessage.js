c1fc86a2bd6f7921079c2c749a65fbd23b5b000d
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-04-27 18:02:41 -0500

    saves json object into UserStat class

--- receiveMessage.js
+++ receiveMessage.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 const freeclimbSDK = require('@freeclimb/sdk')
+const axios = require('axios');
 
 const port = process.env.PORT || 3000
 const accountId = process.env.ACCOUNT_ID
@@ -16,8 +17,28 @@ app.post('/incomingSms', (req, res) => {
   const name = verify_name(req['body']['text'])
   if (name == null) {
     freeclimb.api.messages.create(from, to, 'You\'ve enterted an invalid Epic name. Please try again.').catch(err => {console.log(err)})
-  } else {
-    freeclimb.api.messages.create(from, to, `Good afternoon, Mr. ${name}`).catch(err => {console.log(err)})
+  } 
+  else {
+    const response = axios.get('https://fortnite-api.com/v1/stats/br/v2', {
+      params: {
+        name: `${name}`
+      }
+    });
+      response.then(function (response) {
+        // handle success
+        console.log('````````')
+        console.log('Response')
+        //freeclimb.api.messages.create(from, to, response['data']['data']).catch(err => {console.log(err)})
+        const userStats = new UserStats(response['data']['data'])
+        const msg = `Name: ${userStats.name}`
+        console.log(msg)
+
+      })
+      .catch(function (error) {
+        // handle error
+        freeclimb.api.messages.create(from, to, error['response']['data']['error']).catch(err => {console.log(err)})
+        
+      })
   }
 })
 
@@ -39,3 +60,20 @@ function verify_name(str){
     return str
   }
 }
+
+class UserStats {
+  constructor(obj){
+    this.name = obj['account']['name']
+    this.level = obj['battlePass']['level']
+    this.overallScore = obj['stats']['all']['overall']['score']
+    this.overallWins = obj['stats']['all']['overall']['wins']
+    this.overallKills = obj['stats']['all']['overall']['kills']
+    this.overallKillsPerMatch = obj['stats']['all']['overall']['killsPerMatch']
+    this.overallDeaths = obj['stats']['all']['overall']['deaths']
+    this.overallKD = obj['stats']['all']['overall']['kd']
+    this.overallMatches = obj['stats']['all']['overall']['matches']
+    this.overallWinRate = obj['stats']['all']['overall']['winRate']
+    this.overallMinutes = obj['stats']['all']['overall']['minutesPlayed']
+  }
+}
+
